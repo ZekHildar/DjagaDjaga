73e145f134e29851f52d5fd7561177f664e52971
Daniil Ermoshkin <ermoshkin.daniil@mail.ru>
2023-04-25 00:36:07 +0300

    УЖас

--- DjagoHospital/patients/forms.py
+++ DjagoHospital/patients/forms.py
@@ -26,10 +26,14 @@ class AddMedicForm(forms.ModelForm):
     # slug = forms.SlugField(label='URL', max_length=255)
 
 class RegisterUserForm(UserCreationForm):
-     username = forms.CharField(label='Логин')
-     email = forms.EmailField(label='E-mail')
-     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput())
-     password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput())
+     username = forms.CharField(label='')
+    #  Логин
+     email = forms.EmailField(label='')
+    #  E-mail
+     password1 = forms.CharField(label='', widget=forms.PasswordInput())
+    #  Пароль
+     password2 = forms.CharField(label='', widget=forms.PasswordInput())
+    #  Повтор пароля
      class Meta:
          model = User
          fields = ('username', 'email', 'password1', 'password2')
